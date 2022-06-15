6cc9c4e17f82e357f093b248602118228604e459
Ikonera <gabrielmlt@protonmail.ch>
2022-06-15 12:49:29 +0200

    fix(ws): remove platform-ws in favor of socket.io

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost'],
+    origin: ['http://localhost:3010'],
+    allowedHeaders: [
+        "Access-Control-Allow-Origin"
+    ]
   });
-  app.useWebSocketAdapter(new WsAdapter(app)); // REQUIRED
   await app.listen(4600);
 }
 bootstrap();
