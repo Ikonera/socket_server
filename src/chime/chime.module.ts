099c4f62f252c204fbabdd3aa9603a3573c37e64
Ikonera <gabrielmlt@protonmail.ch>
2022-06-27 16:12:42 +0200

    fix: solved direct meeting disconnection & add .env

--- src/chime/chime.module.ts
+++ src/chime/chime.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChimeService } from './chime.service';
 import { ChimeController } from './chime.controller';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
+  imports: [ConfigModule],
   providers: [ChimeService],
-  controllers: [ChimeController]
+  controllers: [ChimeController],
 })
 export class ChimeModule {}
--- src/chime/chime.service.ts
+++ src/chime/chime.service.ts
@@ -1,28 +1,48 @@
 import { Injectable } from '@nestjs/common';
-import { Chime, Endpoint } from "aws-sdk"
+import { Chime, Endpoint, Credentials } from "aws-sdk"
 import { v4 as uuidv4 } from "uuid"
+import {ConfigService} from "@nestjs/config"
+import {Logger} from "@nestjs/common"
 
 @Injectable()
 export class ChimeService {
 	private chime: Chime
+	private credentials: Credentials
+	private logger: Logger
+	private uuid: uuidv4
 
-	constructor() {
-		this.chime = new Chime({ region: "eu-west-3" })
-		this.chime.endpoint = new Endpoint("https://service.chime.aws.amazon.com")
+	constructor(private readonly configService: ConfigService) {
+
+		this.credentials = new Credentials(
+			this.configService.get("aws_access_key_id"),
+			this.configService.get("aws_secret_access_key"),
+			null
+		)
+		this.chime = new Chime({ region: "us-east-1" })
+		this.chime.endpoint = new Endpoint("https://service.chime.aws.amazon.com/console")
+		this.logger = new Logger("ChimeService")
+		this.uuid = uuidv4()
 	}
 
 	getMeetingResponse = async () => {
-		return await this.chime.createMeeting({
-			ClientRequestToken: uuidv4(),
-			MediaRegion: "eu-west-3",
+		const response = await this.chime.createMeeting({
+			ClientRequestToken: this.uuid,
+			MediaRegion: "us-east-1",
 		}).promise()
+		this.logger.log("Get meeting response : ")
+		this.logger.debug(response)
+		return response
 	}
 
 	getAttendeeResponse = async () => {
 		const meetingResponse = await this.getMeetingResponse()
-		return await this.chime.createAttendee({
+		const response = await this.chime.createAttendee({
 			MeetingId: meetingResponse.Meeting.MeetingId,
-			ExternalUserId: uuidv4()
+			ExternalUserId: this.uuid
 		}).promise()
+		this.logger.log("Get attendee response : ")
+		this.logger.debug(response)
+		return response
 	}
+
 }
