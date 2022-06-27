61c554ab6103bd5cf78d5949e94a7f6523e77f60
Ikonera <gabrielmlt@protonmail.ch>
2022-06-27 16:12:56 +0200

    update: add config to map env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChimeModule } from './chime/chime.module';
+import {ConfigModule} from "@nestjs/config";
+import config from "./config/config"
 
 @Module({
-  imports: [ChimeModule],
+  imports: [
+      ChimeModule,
+      ConfigModule.forRoot({
+        load: [config]
+      })],
   controllers: [AppController],
   providers: [AppService],
 })
